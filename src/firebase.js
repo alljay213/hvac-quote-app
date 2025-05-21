61390d5e79547f95907b13392ceba5964bd1cad7
All Jay Gaco <alljaygaco@hotmail.com>
2025-05-21 19:53:08 -0400

    feat: add Firebase initialization and Firestore export

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // ✅ Required for Firestore
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCeamHKKPkraR3fLkvZcenbARxzv-vi_8Y",
+  authDomain: "hvacquoteapp.firebaseapp.com",
+  projectId: "hvacquoteapp",
+  storageBucket: "hvacquoteapp.firebasestorage.app",
+  messagingSenderId: "879782464929",
+  appId: "1:879782464929:web:8b5568887e06550aa9cfe0",
+  measurementId: "G-MNV9FMBNKD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firestore instance
+const db = getFirestore(app);
+export { db };
