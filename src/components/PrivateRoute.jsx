da830a9f47022823d16aa45424a09339015cb588
All Jay Gaco <alljaygaco@hotmail.com>
2025-05-22 16:27:00 -0400

    fix: update redirect path in PrivateRoute to point to login page

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const { currentUser } = useAuth();
 
-  return currentUser ? children : <Navigate to="/login" replace />;
+  return currentUser ? children : <Navigate to="/" />;
 }
